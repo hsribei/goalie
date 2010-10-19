1ee000da5c4eeb26d8f19f3ec2f041ef536b4c6e
Adam Cooper <adam.cooper@gmail.com>
2010-10-19 10:56:56 -0700

    Passing down the config setting for 'consider_all_requests_local' to the Goalie::CustomErrorPages initializer

--- lib/goalie/rails.rb
+++ lib/goalie/rails.rb
@@ -2,7 +2,7 @@ module Goalie
   class Engine < Rails::Engine
 
     initializer "goalie.add_middleware" do |app|
-      app.middleware.insert_after 'ActionDispatch::RemoteIp', Goalie::CustomErrorPages
+      app.middleware.insert_after 'ActionDispatch::RemoteIp', Goalie::CustomErrorPages, app.config.consider_all_requests_local
       app.middleware.delete 'ActionDispatch::ShowExceptions'
     end
 
